e1dfb7942433ce62213aa1f38611798f0b130105
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-06-24 15:20:28 -0600

    changed footer

--- ww-tracker/src/components/Footer.tsx
+++ ww-tracker/src/components/Footer.tsx
@@ -2,7 +2,6 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-primary py-6 flex flex-col items-center justify-center">
       <h2 className="text-2xl">Wuthering Waves Tracker</h2>
-      <p className="m-0">Developed by Alessandro Baldassarre</p>
     </footer>
   );
 };
